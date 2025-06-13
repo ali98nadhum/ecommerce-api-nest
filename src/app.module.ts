b6d8e5381e20acadcebd75ecfff010f0dddf2868
Ali nadhum <alinadhum1998@gmail.com>
2025-06-13 14:00:27 +0300

    create user model v1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest-ecommerce')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest-ecommerce'), UserModule],
   controllers: [],
   providers: [],
 })
