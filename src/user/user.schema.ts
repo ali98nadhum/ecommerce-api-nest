6925192ce1c386fe147b307378e6eebfda8ccf9c
Ali nadhum <alinadhum1998@gmail.com>
2025-06-13 14:54:24 +0300

    create userModel v2

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,13 +1,78 @@
-
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema({timestamps:true})
+@Schema({ timestamps: true })
 export class User {
-  @Prop()
+  
+  @Prop({
+    type: String,
+    required: true,
+    minlength: [3, 'Name must be at least 3 characters'],
+    maxlength: [30, 'Name must be at most 30 characters'],
+  })
   name: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    min: [3, 'Password must be at least 3 characters'],
+    max: [20, 'Password must be at most 20 characters'],
+  })
+  password: String;
+
+  @Prop({
+    type: String,
+    required: true,
+    enum: ['user', 'admin'],
+    default: 'user',
+  })
+  role: string;
+
+  @Prop({
+    type: String,
+  })
+  avatar: string;
+
+  @Prop({
+    type: Number,
+  })
+  age: number;
+
+  @Prop({
+    type: Number,
+  })
+  phoneNumber: number;
+
+  @Prop({
+    type: String,
+  })
+  address: string;
+
+  @Prop({
+    type: Boolean,
+    enum: [true, false],
+  })
+  active: boolean;
+
+  @Prop({
+    type: String,
+  })
+  verificationCode: string;
+
+  @Prop({
+    type: String,
+    enum: ['male', 'female'],
+  })
+  gender: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
